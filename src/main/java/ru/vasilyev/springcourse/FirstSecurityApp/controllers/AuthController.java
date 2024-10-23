ac0254cda66ac5fd32ae81fed9fb16cdf5f45acd
Alexander Vasilyev <sasha.vasilevvs@yandex.ru>
2024-10-23 19:57:58 +0300

    Added auth form

---
+++ src/main/java/ru/vasilyev/springcourse/FirstSecurityApp/controllers/AuthController.java
@@ -0,0 +1,23 @@
+package ru.vasilyev.springcourse.FirstSecurityApp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.vasilyev.springcourse.FirstSecurityApp.models.Person;
+
+@Controller
+@RequestMapping("/auth")
+public class AuthController {
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "auth/login";
+    }
+
+    @GetMapping("/new")
+    public String registrationPage(@ModelAttribute("person") Person person) {
+        return "auth/registration";
+    }
+
+}
